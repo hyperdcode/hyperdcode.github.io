aad866508bbd36439e48ecfeb74cb7aef3fa5569
Fakeclover <davidpoma030@outlook.es>
2021-01-02 17:18:56 +0100

    Añadi una funcion de typeo ,con la libreria cdn

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation script
+var typed = new Typed(".typing", {
+  strings: ["Fieras", "Mastodontes", "Tiburones", "Cachalotes", "PLATZI"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true
+});
